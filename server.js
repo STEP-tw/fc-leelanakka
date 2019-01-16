2ce308de64a8e9c399fa515a8303ca028cc27654
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2019-01-16 12:27:51 +0530

    added flowerCatalog.html file
    now the server will run on 8080 port and requests the html file
    now,we can see one image and header

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const app = require('./src/app.js');
+const PORT = process.env.PORT || 8080;
+
 
 // Do not change the port value
 // If you want to use a different port
 // set it as an environment variable.
-const PORT = process.env.PORT || 8080;
 
 let server = http.createServer(app);
 server.listen(PORT, () => console.log('listening on ', PORT));
--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
+const fs = require("fs");
+
+const readFiles = function(req, res, fileName) {
+  fs.readFile(fileName, (err, data) => {
+    if (err) {
+      res.statusCode = 404;
+      res.end();
+      return;
+    } else {
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.statusCode = 200;
+      res.end();
+    }
+  });
+};
+
 const app = (req, res) => {
-  res.statusCode = 404;
-  res.end();
+  readFiles(req, res, req.url.slice(1) || "flowerCatalog.html");
 };
 
 // Export a function that can act as a handler
