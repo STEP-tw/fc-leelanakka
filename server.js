381188cd6abd6df1c1d2a5d5ec3d2e4255e2e641
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2019-01-17 14:49:25 +0530

    introduced guestBook.html file
    when we click on guest book it will redirect to guestbook page

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const http = require('http');
-const app = require('./src/app.js');
+const http = require("http");
+const app = require("./src/app.js");
 const PORT = process.env.PORT || 8080;
-
 
 // Do not change the port value
 // If you want to use a different port
 // set it as an environment variable.
 
 let server = http.createServer(app);
-server.listen(PORT, () => console.log('listening on ', PORT));
+server.listen(PORT, () => console.log("listening on ", PORT));
